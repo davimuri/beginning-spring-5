8b7cc326ae0e44090fb0cc7f1c0991c169ac85e3
David Murillo <davimuri@gmail.com>
2020-08-29 17:51:53 +0200

    Add chapter10

---
+++ chapter10/src/main/java/com/bsg5/chapter10/GatewaySecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.bsg5.chapter10;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class GatewaySecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
